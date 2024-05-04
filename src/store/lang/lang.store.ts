398ce690c2ce6c3035f7d296874e7a28edbc3026
M.Zayniddin <clon3.secur1ty@gmail.com>
2024-05-05 01:55:30 +0500

    fix(lang): saved changed lang to the localStorage

--- src/store/lang/lang.store.ts
+++ src/store/lang/lang.store.ts
@@ -1,12 +1,16 @@
 import { create } from 'zustand';
 import { LANG } from '@/constants';
-import { createSelectors, getLocalStorage } from '@/utils';
+import { createSelectors, getLocalStorage, setLocalStorage } from '@/utils';
 import { TLang, TLangStore } from '@/types/lang.types';
 import { lng } from '@/i18n';
 
 const useTheme = create<TLangStore>()((set) => ({
   lang: getLocalStorage(LANG) || lng,
-  setLang: (lang: TLang) => set(() => ({ lang })),
+  setLang: (lang: TLang) =>
+    set(() => {
+      setLocalStorage(LANG, lang);
+      return { lang };
+    }),
 }));
 
 export const useLangStore = createSelectors(useTheme);
